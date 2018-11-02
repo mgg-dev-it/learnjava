0f9b1886f473d80d767a24c3f79ec847400d687e
mgg-dev-it <44374657+mgg-dev-it@users.noreply.github.com>
2018-11-02 21:49:50 +0100

    .

--- src/main/java/mggdevit/learnjava/stream/LearnStream.java
+++ src/main/java/mggdevit/learnjava/stream/LearnStream.java
@@ -4,6 +4,10 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
+import java.util.Random;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 import mggdevit.learnjava.SampleData;
 
@@ -16,8 +20,14 @@ import mggdevit.learnjava.SampleData;
  */
 public abstract class LearnStream {
 
-    private static void dumpStringStream(Stream<String> stream) {
-        dumpStringStream(null, stream);
+//    private static void dumpStringStream(Stream<String> stream) {
+//        dumpStringStream(null, stream);
+//    }
+
+    private static void dumpIntegerStream(String sHead, Stream<Integer> stream) {
+        Integer[] integerArray = stream.toArray(Integer[]::new);
+        //System.out.println("stream dump: " + Arrays.toString(stringArray));
+        System.out.println((sHead == null ? "" : sHead + ": ") + Arrays.toString(integerArray));
     }
 
     private static void dumpStringStream(String sHead, Stream<String> stream) {
@@ -26,9 +36,9 @@ public abstract class LearnStream {
         System.out.println((sHead == null ? "" : sHead + ": ") + Arrays.toString(stringArray));
     }
 
-    private static void dumpStringOptional(Optional<String> optional) {
-        dumpStringOptional(null, optional);
-    }
+//    private static void dumpStringOptional(Optional<String> optional) {
+//        dumpStringOptional(null, optional);
+//    }
 
     private static void dumpStringOptional(String sHead, Optional<String> optional) {
         if (optional.isPresent()) {
@@ -51,6 +61,18 @@ public abstract class LearnStream {
         dumpStringStream("Stream created from array with 'Stream.of(T... values)'", Stream.of(sampleData));
         dumpStringStream("Stream created from array with 'Arrays.stream(T[] array)'", Arrays.stream(sampleData));
         dumpStringStream("Stream created from List<String> with 'Collection.stream()'", stringList.stream());
+        dumpStringStream("Stream created with 'Stream.builder()'", Stream.<String>builder().add("a").add("b").add("c").build());
+        dumpStringStream("Stream created with 'Stream.generate()'", Stream.generate(() -> "a").limit(10));
+        dumpIntegerStream("Stream created with 'Stream.iterate()'", Stream.iterate(10, n -> n + 2).limit(10));
+        dumpIntegerStream("Stream created with 'IntStream.range(1, 3)'", IntStream.range(1, 3).boxed());
+        dumpIntegerStream("Stream created with 'IntStream.rangeClosed(1, 3)'", IntStream.rangeClosed(1, 3).boxed());
+        System.out.println("LongStream");
+        System.out.println("DoubleStream");
+        dumpIntegerStream("Stream created with 'new Random().ints(10, 10, 20)'", new Random().ints(10, 10, 20).boxed());
+        System.out.println("Random.doubles");
+        System.out.println("Random.longs");
+        dumpIntegerStream("Stream created with '\"abc\".chars()'", "abc".chars().boxed());
+        dumpStringStream("Stream created with 'Pattern.compile(\", \").splitAsStream(\"a, b, c\")'", Pattern.compile(", ").splitAsStream("a, b, c"));
 
         System.out.println("");
         System.out.println("Stream info");
@@ -64,6 +86,7 @@ public abstract class LearnStream {
         System.out.println("Stream to stream");
         System.out.println("----------------");
         dumpStringStream("Five elements of the stream", Stream.of(sampleData).limit(5));
+        dumpStringStream("Filtered stream with 'filter(element -> element.contains(\"b\")'", Stream.of(sampleData).filter(element -> element.contains("b")));
 
         System.out.println("");
         System.out.println("Optionals");
@@ -72,6 +95,11 @@ public abstract class LearnStream {
         dumpStringOptional("Minimum element of the stream ", Stream.of(sampleData).min(String::compareTo));
         dumpStringOptional("Maximum element of the stream ", Stream.of(sampleData).max(String::compareTo));
 
+        System.out.println("");
+        System.out.println("---");
+        List<String> list2 = Stream.of("a", "b", "c").filter(element -> element.contains("b")).collect(Collectors.toList());
+        System.out.println("List<String> made from Stream with 'collect(Collectors.toList()': " + list2.toString());
+        
         System.out.println("");
         System.out.println("---");
         dumpStringStream("Ordered stream", Stream.of(sampleData).sorted((String s1, String s2) -> (s1.compareTo(s2))));
